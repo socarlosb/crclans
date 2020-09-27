73b8b700dcfd7016d0a7c58d305714590ecf23a1
Carlos <socarlosb@gmail.com>
2020-09-27 01:31:33 +0100

    remove .next folder

--- .next/static/hPfrTyioiWaEY9Ue22uEJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F3c37d28abfa1b169c483.css","static\u002Fchunks\u002Fpages\u002Findex-63c1b63b87d2a7ba572b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
