9deb58d02c5b3316f27ca4efc233e18747c6345b
Carlos <socarlosb@gmail.com>
2020-09-27 01:22:11 +0100

    update with nextjs

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # crclans
+
 A simple app that gives an overview of Clash Royale gaming communities
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+const Home = () => (
+  <section className={styles.container}>
+    <h2>Placeholder</h2>
+  </section>
+);
+
+export default Home;
